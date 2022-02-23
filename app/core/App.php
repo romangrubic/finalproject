62b415d6884106509a8b0e45190dc39f57d309ce
Roman Grubic <grubic.roman@gmail.com>
2022-02-23 13:39:10 +0100

    Creating mechanism to call controller -> method

--- app/core/App.php
+++ app/core/App.php
@@ -4,8 +4,33 @@ class App
 {
 public static function start()
     {
+        // Taking the user url and splitting it into parts
         $route=Request::getRoute();
+        $parts=explode(DIRECTORY_SEPARATOR, $route);
 
-        echo $route;
+        // Next part takes parts[1] and adds Controller so that it can point to a certain controller in app/controller folder
+        $class='';
+        if(!isset($parts[1]) || $parts[1]===''){
+            $class='Index';
+        } else {
+            $class=ucfirst($parts[1]);
+        }
+        $class .= 'Controller';
+
+        // This one takes parts[2] and sets the name of the method
+        $method='';
+        if(!isset($parts[2]) || $parts[2]==''){
+            $method='index';
+        } else {
+            $method=lcfirst($parts[2]);
+        }
+
+        // Now we check if the class and method in the same class exist and activate it. Otherwise we show 404error page.
+        if(class_exists($class) && method_exists($class, $method)){
+            $instance=new $class();
+            $instance->$method();
+        } else {
+            echo $class . '->' . $method . '() does not exist. 404 error.';
+        }
     }
 }
