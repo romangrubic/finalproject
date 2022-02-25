44773807af363b6e346361496e3bbff34590e8a8
Roman Grubic <grubic.roman@gmail.com>
2022-02-25 15:14:55 +0100

    Adding a switch case to config.php depending on server address and an indicator for development environment.

--- app/config.php
+++ app/config.php
@@ -1,6 +1,25 @@
 <?php
 
+
+switch ($_SERVER['SERVER_ADDR']) {
+    // Development or local
+    case '127.0.0.1':
+        $dev = true;
+        $url = 'http://romangrubic.com/';
+        break;
+    // Production
+    case '185.62.73.200':
+        $dev = false;
+        $url = 'https://mojatrgovina.online/';
+        break;
+
+    default:
+        echo 'Not valid server adress.';
+        break;
+}
+
 return [
-    'url'=>'http://romangrubic.com/',
-    'appName'=>'Moja Trgovina'
+    'dev' => $dev,
+    'url' => $url,
+    'appName' => 'Moja Trgovina'
 ];
