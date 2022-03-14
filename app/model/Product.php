d81f0b539330b6005d6ade78cf1ddfe94e02652d
Roman Grubic <grubic.roman@gmail.com>
2022-03-14 14:27:32 +0100

    Product model for insert, update and delete

--- app/model/Product.php
+++ app/model/Product.php
@@ -35,4 +35,104 @@ class Product
         $query->execute();
         return $query->fetchall();
     }
+
+    public static function create($parameters)
+    {
+        $connection = DB::getInstance();
+        $connection->beginTransaction();
+        $query = $connection->prepare('
+        
+                insert into product (name, description, category, price, inventoryquantity, dateadded)
+                values (:name, :description, :category, :price, :inventoryquantity, now())
+        
+        ');
+        $query->execute([
+            'name'=>$parameters['name'],
+            'description'=>$parameters['description'],
+            'category'=>$parameters['category'],
+            'price'=>$parameters['price'],
+            'inventoryquantity'=>$parameters['inventoryquantity'],
+        ]);
+
+        $lastProductId=$connection->lastInsertId();
+
+        $query = $connection->prepare('
+        
+                insert into productimage (product, imageurl, dateadded)
+                values (:product, :imageurl, now())
+        
+        ');
+        $query->execute([
+            'product'=>$lastProductId,
+            'imageurl'=>$parameters['imageurl']
+            
+        ]);
+
+        $connection->commit();
+    }
+
+    public static function update($parameters)
+    {
+        $connection = DB::getInstance();
+        $connection->beginTransaction();
+        $query = $connection->prepare('
+        
+            update product set
+            name=:name,
+            description=:description,
+            category=:category,
+            price=:price,
+            inventoryquantity=:inventoryquantity
+            where id=:id
+                
+        ');
+        $query->execute([
+            'name'=>$parameters['name'],
+            'description'=>$parameters['description'],
+            'category'=>$parameters['category'],
+            'price'=>$parameters['price'],
+            'inventoryquantity'=>$parameters['inventoryquantity'],
+            'id'=>$parameters['id'],
+        ]);
+
+        $query = $connection->prepare('
+        
+            update productimage set
+            imageurl=:imageurl
+            where product=:product
+        
+        ');
+        $query->execute([
+            'product'=>$parameters['id'],
+            'imageurl'=>$parameters['imageurl']
+            
+        ]);
+
+        $connection->commit();
+    }
+
+    public static function delete($id)
+    {
+        $connection = DB::getInstance();
+        $connection->beginTransaction();
+        $query = $connection->prepare('
+        
+            delete from productimage where product=:id
+                
+        ');
+        $query->execute([
+            'id'=>$id
+        ]);
+
+        $query = $connection->prepare('
+        
+            delete from product where id=:id
+        
+        ');
+        $query->execute([
+            'id'=>$id
+        ]);
+
+        $connection->commit();
+    }
 }
