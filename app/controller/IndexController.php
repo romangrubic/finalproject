b289a68d40eb3db664f275cf7a148bfaab4cb974
Roman Grubic <grubic.roman@gmail.com>
2022-03-16 13:26:44 +0100

    Adding admin navbar.

--- app/controller/IndexController.php
+++ app/controller/IndexController.php
@@ -4,6 +4,10 @@ class IndexController extends Controller
 {
     public function index()
     {
+        if (isset($_SESSION['authorized']->user_role) && ($_SESSION['authorized']->user_role == 'admin' || $_SESSION['authorized']->user_role == 'oper')) {
+            $this->view->render('admin/index');
+            return;
+        }
         $this->view->render('index');
     }
 }
