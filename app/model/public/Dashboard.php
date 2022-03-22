4124259e048e9a6e0e36f037863b8fd29a82ed64
Roman Grubic <grubic.roman@gmail.com>
2022-03-22 23:20:12 +0100

    Adding Orders to Customer Dashboard panel.

--- app/model/public/Dashboard.php
+++ app/model/public/Dashboard.php
@@ -8,8 +8,10 @@ class Dashboard
         $connection = DB::getInstance();
         $query = $connection->prepare('
 
-            select * 
-            from shoppingorder 
+            select a.id, a.dateadded, c.id, c.name, b.price, b.quantity
+            from shoppingorder a
+            inner join cart b on a.id=b.shoppingorder
+            inner join product c on b.product=c.id
             where customer=:customerId
             
         ');
